d929a611de4f48b8f2293b7d0e28cbaf8c2e5638
Rachel West <westrachel27@gmail.com>
2023-03-10 15:59:00 -0600

    Refactored utilities to try to separate concerns more and not mix levels of abstractions
    Co-authored-by: Albert Dorfman  <albertdorfman@gmail.com>
    Co-authored-by: Ginni Pinckert <gcpinckert@gmail.com>
    Co-authored-by: Luke Oguro <lukeoguro@gmail.com>
    Co-authored-by: Rachel West <westrachel27@gmail.com>

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -5,6 +5,7 @@ const K6_CR_FINAL = "cr_final.yaml";
 const CLUSTER_NAME = "edamame";
 const LOAD_GEN_NODE_GRP = "load-generators";
 const K6_TEST_POD_REGEX = 'k6-edamame-test-[0-9]{1,}';
+const EBS_CSI_DRIVER_REGEX = "ebs-csi-controller-sa.*AmazonEKS_EBS_CSI_DriverRole";
 
 export {
   NUM_VUS_PER_POD,
@@ -12,5 +13,6 @@ export {
   K6_CR_FINAL,
   CLUSTER_NAME,
   LOAD_GEN_NODE_GRP,
-  K6_TEST_POD_REGEX
+  K6_TEST_POD_REGEX,
+  EBS_CSI_DRIVER_REGEX
 };
--- src/utilities/kubectl.js
+++ src/utilities/kubectl.js
@@ -1,67 +1,36 @@
-import fs from "fs";
-import yaml from "js-yaml";
 import { NUM_VUS_PER_POD, K6_CR_TEMPLATE, K6_CR_FINAL } from "./constants.js";
 import { promisify } from "util";
 import child_process from "child_process";
 import fullPath from "./path.js";
-import ora from "ora";
-import cli from "./cli.js";
-import eksctl from "./eksctl.js";
 
 const exec = promisify(child_process.exec);
 const k6OpDeployPath = fullPath('../k6-operator/deployment');
 
 const kubectl = {
-  deployK6Op(spinner) {
-   spinner.text = "Deploying K6 operator...";
-   return ( 
-    exec(`cd ${k6OpDeployPath} && cd .. && make deploy`)
-      .then(() => cli(spinner, 'Deployed k6 operator to cluster'))
-    );
+  deployK6Operator() {
+    return exec(`cd ${k6OpDeployPath} && cd .. && make deploy`);
   },
 
-  phaseOutK6(configMapName) {
-    return (
-      exec(`kubectl delete -f ${k6OpDeployPath}/statsite.yaml`)
-        .then(() => exec(`kubectl delete configmap ${configMapName} -n default`))
-        .then(() => eksctl.scaleLoadGenNodes(0))
-        .then(() => exec(`kubectl delete -f ${k6OpDeployPath}/${K6_CR_FINAL}`))
-    );
+  deleteConfigMap(name) {
+    return exec(`kubectl delete configmap ${name} -n default`);
   },
 
-  launchK6Test(testPath, numVus, configMapName, spinner) {
-    this.createK6CustomResource(testPath, numVus, configMapName);
-    const numNodes = this.parallelism(numVus);
+  createConfigMap(name, path) {
+    return exec(`kubectl create configmap ${name} --from-file ${path}`);
+  },
 
-    return (
-      exec(`kubectl apply -f ${k6OpDeployPath}/statsite.yaml`)
-        .then(() => exec(`kubectl create configmap ${configMapName} --from-file ${testPath}`))
-        .then(() => eksctl.scaleLoadGenNodes(numNodes))
-        .then(() => exec(`kubectl apply -f ${k6OpDeployPath}/${K6_CR_FINAL}`))
-        .then(() => cli(spinner, "Starting k6 load test..."))
-    );
+  deleteManifest(path) {
+    return exec(`kubectl delete -f ${path}`);
+  },
+
+  applyManifest(path) {
+    return exec(`kubectl apply -f ${path}`);
   },
 
   getPods() {
     return exec(`kubectl get pods`);
-  },
-
-  createK6CustomResource(path, numVus, configMapName) {
-    const cr = yaml.load(fs.readFileSync(`${k6OpDeployPath}/${K6_CR_TEMPLATE}`, 'utf-8'));
-    cr.spec.parallelism = this.parallelism(numVus);
-    cr.spec.script.configMap.name = configMapName;
-    cr.spec.script.configMap.file = this.testFileName(path);
-    fs.writeFileSync(`${k6OpDeployPath}/${K6_CR_FINAL}`, yaml.dump(cr));
-  },
-  
-  testFileName(path) {
-    const pathItems = path.split('/');
-    return pathItems[pathItems.length -1];
-  },
-
-  parallelism(numVus) {
-    return Math.ceil(numVus / NUM_VUS_PER_POD);
   }
+
 };
 
 export default kubectl;
