cb6f6f683ccc83360b53ade5008c9fcb5796e6d0
Luke Oguro <lukeoguro@gmail.com>
2023-03-13 18:05:05 -0400

    Improve edamame init logs
    Co-authored-by: Albert Dorfman  <albertdorfman@gmail.com>
    Co-authored-by: Ginni Pinckert <gcpinckert@gmail.com>
    Co-authored-by: Luke Oguro <lukeoguro@gmail.com>
    Co-authored-by: Rachel West <westrachel27@gmail.com>

--- src/commands/init.js
+++ src/commands/init.js
@@ -2,22 +2,29 @@ import cluster from "../utilities/cluster.js";
 import Spinner from "../utilities/spinner.js";
 import userInput from "../utilities/userInput.js";
 
-const init = () => {
-  const spinner = new Spinner("Creating Edamame Cluster...");
-  cluster.create()
-    .then(() => spinner.update("Configuring EBS Credentials..."))
-    .then(() => cluster.configureEBSCreds())
-    .then(() => userInput.processPassword())
-    .then(() => spinner.update("Deploying Grafana, Postgres, & K6 Operator..."))
-    .then(() => cluster.deployServersK6Op())
-    .then(grafanaUrl => {
-      spinner.succeed(
-        `Cluster configured. ` +
-        `Please load the grafana dashboard at: ${grafanaUrl} ` + 
-        `and then run a load test.`)
-    })
-    .catch(err => spinner.fail(`Error creating cluster: ${err}`));
-};
+const init = async () => {
+  const spinner = new Spinner("Creating Edamame cluster... (this may take up to 20 minutes)");
+
+  try {
+    await cluster.create();
+    spinner.succeed("Successfully created Edamame cluster.");
+
+    spinner.info("Configuring EBS credentials...");
+    spinner.start();
+    await cluster.configureEBSCreds();
+    spinner.succeed("Successfully configured EBS credentials.");
+
+    await userInput.processPassword()
+
+    spinner.info("Deploying Grafana, Postgres, & K6 Operator...");
+    spinner.start();
+    const grafanaUrl = await cluster.deployServersK6Op();
+    spinner.info(`Please find your Grafana dashboard at: ${grafanaUrl}.`);
+    spinner.succeed("Cluster configured. Welcome to Edamame!");
+  } catch (err) {
+    spinner.fail(`Error creating cluster: ${err}`);
+  }
+}
 
 export {
   init
