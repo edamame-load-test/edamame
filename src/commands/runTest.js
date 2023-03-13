a1bea30362c99a5ee06df33e26197ba20a62cdb9
Luke Oguro <lukeoguro@gmail.com>
2023-03-13 18:25:28 -0400

    Update run test logs
    Co-authored-by: Albert Dorfman  <albertdorfman@gmail.com>
    Co-authored-by: Ginni Pinckert <gcpinckert@gmail.com>
    Co-authored-by: Luke Oguro <lukeoguro@gmail.com>
    Co-authored-by: Rachel West <westrachel27@gmail.com>

--- src/commands/runTest.js
+++ src/commands/runTest.js
@@ -4,23 +4,33 @@ import Spinner from "../utilities/spinner.js";
 import cluster from "../utilities/cluster.js";
 import manifest from "../utilities/manifest.js";
 
-const runTest = (testPath) => {
-  const numVus = manifest.numVus(testPath);
-  if (numVus > 0) {
-    const spinner = new Spinner("Distributing k6 load test...");
-    
-    cluster.launchK6Test(testPath, numVus)
-      .then(() => spinner.update("Running k6 load test..."))
-      .then(() => loadGenerators.pollUntilAllComplete(numVus))
-      .then(() => spinner.update("All tests completed; Removing load generators."))
-      .then(() => cluster.phaseOutK6())
-      .then(() => spinner.succeed("Removed load generating components from cluster."))
-      .catch(err => spinner.fail(`Error running test: ${err}`));
-  } else {
-    console.log(
-      `Either couldn't find k6 test script at: ${testPath} ` +
-      `Or the file specifies 0 total number of vus.`
-    );
+const runTest = async (testPath) => {
+  const spinner = new Spinner("Reading test script...");
+
+  try {
+    const numVus = manifest.numVus(testPath);
+    if (numVus === 0) {
+      throw new Error(`Either couldn't find k6 test script at path or the file specifies 0 total number of VUs.`);
+    }
+    spinner.succeed(`Successfully read test script.`);
+
+
+    spinner.info(`Initializing load test with ${numVus}...`);
+    spinner.start();
+    await cluster.launchK6Test(testPath, numVus);
+    spinner.succeed("Successfully initialized load test.");
+
+    spinner.info("Running load test...");
+    spinner.start();
+    await loadGenerators.pollUntilAllComplete(numVus);
+    spinner.succeed("Load test completed.");
+
+    spinner.info("Tearing down load generating resources.");
+    spinner.start();
+    await cluster.phaseOutK6();
+    spinner.succeed("Successfully removed load generating resources from cluster.");
+  } catch (err) {
+    spinner.fail(`Error running test: ${err}`);
   }
 };
 
