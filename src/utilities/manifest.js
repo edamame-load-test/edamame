59cafd6d0430265d3606744dc0b18c1363c02c6a
Albert Dorfman <albertdorfman@gmail.com>
2023-03-14 21:43:27 -0400

    Using secrets to inform username and pw for grafana
    Co-authored-by: Ginni Pinckert <gcpinckert@gmail.com>
    Co-authored-by: Luke Oguro <lukeoguro@gmail.com>
    Co-authored-by: Rachel West <westrachel27@gmail.com>

--- src/utilities/manifest.js
+++ src/utilities/manifest.js
@@ -4,7 +4,7 @@ import {
   PG_SECRET_FILE,
   GRAF_DS_FILE,
   CLUSTER_NAME,
-  GRAF_JSON_DBS
+  GRAF_JSON_DBS,
 } from "../constants/constants.js";
 import files from "./files.js";
 import fs from "fs";
@@ -17,7 +17,7 @@ const manifest = {
     k6CrData.spec.parallelism = this.parallelism(numVus);
     k6CrData.spec.script.configMap.name = testId;
     k6CrData.spec.script.configMap.file = files.parseNameFromPath(path);
-    envObjs.forEach(obj => {
+    envObjs.forEach((obj) => {
       if (obj.name === "K6_STATSD_NAMESPACE") {
         obj.value = `${testId}.`;
       }
@@ -29,18 +29,18 @@ const manifest = {
   numVus(path) {
     let numVus = 0;
 
-    if (files.exists(path)) {   
+    if (files.exists(path)) {
       let test = files.read(path);
       test = test.split("\n");
 
-      test.forEach(line => {
+      test.forEach((line) => {
         let vus = line.match("vus: [0-9]{1,}");
         let target = line.match("target: [0-9]{1,}");
 
         if (vus) {
-          numVus = this.maxNumVus(numVus, vus[0])
+          numVus = this.maxNumVus(numVus, vus[0]);
         } else if (target) {
-          numVus = this.maxNumVus(numVus, target[0])
+          numVus = this.maxNumVus(numVus, target[0]);
         }
       });
     }
@@ -61,38 +61,31 @@ const manifest = {
     return data.spec.script.configMap.name;
   },
 
-  setPgGrafCredentials(pw) {
+  setPgCredentials(pw) {
     const pgSecret = files.readYAML(PG_SECRET_FILE);
-    const grafCreds = files.readYAML(GRAF_DS_FILE);
-
-    grafCreds.datasources[0].user = `${CLUSTER_NAME}_reader`;
-    grafCreds.datasources[0].secureJsonData.password = pw;
-
     pgSecret.data["psql-username"] = this.base64(CLUSTER_NAME);
     pgSecret.data["psql-password"] = this.base64(pw);
-
     files.write(PG_SECRET_FILE, pgSecret);
-    files.write(GRAF_DS_FILE, grafCreds);
   },
 
   forEachGrafJsonDB(callback) {
     const dirPath = files.path(`/${GRAF_JSON_DBS}`);
     const names = files.fileNames(dirPath);
-    names.forEach(name => {
+    names.forEach((name) => {
       if (name.match(".json")) {
-        const nameNoExt = name.replace("\.json", "").replace("_", "-");
+        const nameNoExt = name.replace(".json", "").replace("_", "-");
         callback(nameNoExt, `${dirPath}/${name}`);
       }
     });
   },
 
   base64(value) {
-    return Buffer.from(value, 'utf8').toString('base64');
+    return Buffer.from(value, "utf8").toString("base64");
   },
 
   parallelism(numVus) {
     return Math.ceil(numVus / NUM_VUS_PER_POD);
-  }
+  },
 };
 
 export default manifest;
